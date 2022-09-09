8d060725202041223f6547be35ad2a994f11aa51
Prashant Kumar <kumarprince2510@gmail.com>
2022-09-09 11:23:33 +0530

    basic-chat: chat related socket event addition

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const http = require('http')
 require('dotenv').config()
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,20 +18,33 @@ app.use(express.static(publicDirectoryPath))
 
 
 /**
- * socket io connectio 
+ * socket io connection 
  */
-let count = 0
 
 io.on('connection', (socket) => {
-    console.log('New WebSocket connection')
+ console.log('New WebSocket connection')
 
-    // creating and sending socket event "countUpdated" to client
-    socket.emit('countUpdated', count)
-    
-    // receiving first then sending socket event to client
-    socket.on('increment', () => {
-        count++
-        io.emit('countUpdated', count)
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
